ba11eeeeb72e0c0fa3b53dac8137eeb7498c9bee
xMISSJ <xmissj96@gmail.com>
2019-12-20 08:47:17 +0100

    Changes: Fixed the RecyclerView overlapping the EditText by adding a Linear Layout (vertical).

--- app/src/main/java/com/example/popularmovies/MainActivity.kt
+++ app/src/main/java/com/example/popularmovies/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.popularmovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.popularmovies.API.Movie
 import com.example.popularmovies.API.MovieApi
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_movie.*
 
 const val api_key = "f0fa9c9cc2005f6f66bff61af5faad51"
 
@@ -25,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.title = "Movie List";
 
         btn_submit.setOnClickListener{
-            viewModel.getPopularMovies(); // Get all the popular movies from the API.
+            onClick();
         }
 
         initViews();
@@ -44,9 +47,24 @@ class MainActivity : AppCompatActivity() {
     private fun initViewModel(){
         viewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
         viewModel.movies.observe(this, Observer {
-            movies.clear()
-            movies.addAll(it)
-            movieAdapter.notifyDataSetChanged()
+            movies.clear();
+            movies.addAll(it);
+            movieAdapter.notifyDataSetChanged();
         })
+    }
+
+    private fun onClick() {
+        var submitYear = etYear.text.toString();
+        val yearRange = 1900..2019
+        if (submitYear.isNotBlank()) {
+            if (submitYear.toInt() !in yearRange) {
+                Toast.makeText(this, "Date must be between 1900 and 2019.", Toast.LENGTH_LONG)
+                    .show();
+            } else {
+                Toast.makeText(this, "Load Movies", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(this, "Please fill in a date.", Toast.LENGTH_LONG).show();
+        }
     }
 }
